812325e228bbc82a661b9d729562e515e4589c85
h2danilo <h2danilo@hotmail.com>
2025-06-03 21:44:37 -0300

    feat(): linkedin scraping

---
+++ third_parties/linkedin.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile"""
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/h2danilo/a67893fbe5354d35ed2d8caa28d17d20/raw/b52d23631d00a9ba9db5f3d981d1503c7f937bed/danilo-valim-scraping.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        # ENROLL WITH COUPON CODE: EDENMARCO
+        # For 20% Discount on all pricing
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ["SCRAPIN_API_KEY"],
+            "linkedInUrl": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params=params,
+            timeout=10,
+        )
+
+    data = response.json().get("person")
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None) and k not in ["certifications"]
+    }
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/danilo-valim/"
+        ),
+    )
